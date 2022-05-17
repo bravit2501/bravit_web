f9f2a3402ff957ec3079fb40951ddd51c67b67e0
bravit2501 <bravitpharmaceutical@gmail.com>
2022-05-17 11:19:27 +0530

    web 7.6

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import "../styles/globals.css";
 import "../styles/Home.module.css";
-import { PresistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-
-import { useStore } from "react-redux";
+import { Provider, useStore } from "react-redux";
 import { wrapper } from "../redux/store";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
@@ -14,11 +11,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Provider store={store}>
-        {/* <PresistGate loading={null} persistor={store.__presistor}> */}
         <Toaster />
         <WhatsAppButton />
         <Component {...pageProps} />
-        {/* </PresistGate> */}
       </Provider>
     </StateContext>
   );
